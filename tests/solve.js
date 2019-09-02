ae443546a8b58637b700e22c71b4a93248c5def8
shadkain <kryzev@gmail.com>
2019-09-02 17:02:47 +0300

    added new tests

--- tests/solve.js
+++ tests/solve.js
@@ -9,4 +9,14 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
 	});
+
+	QUnit.test('Длинные строчки', function (assert) {
+		assert.strictEqual(solve('x - x * 3 + 56 - x + 22 * x', 2), 94);
+		assert.strictEqual(solve('144 - x * x + 12 - 6 * x + 3', 3), 132);
+	});
+
+	QUnit.test('Cкобки', function (assert) {
+		assert.strictEqual(solve('11 - (3 - x) * ((x + 22) + x)', 4), 41);
+		assert.strictEqual(solve('(11 - (26 + x) * x) * x', 7), -1540);
+	});
 });
